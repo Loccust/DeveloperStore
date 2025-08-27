4f961a73bca3c2ced52281195d2ba3ea6dc5b55e
Lucas Costa <scan.lcosta@voeazul.com.br>
2025-08-27 12:25:08 -0300

    get user feature

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Users/GetUser/GetUserResponse.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Users/GetUser/GetUserResponse.cs
@@ -1,39 +1,14 @@
-using Ambev.DeveloperEvaluation.Domain.Enums;
+using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
 
 namespace Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
 
 /// <summary>
 /// API response model for GetUser operation
 /// </summary>
-public class GetUserResponse
+public class GetUserResponse : UserDto
 {
     /// <summary>
-    /// The unique identifier of the user
+    /// The unique identifier of the created user
     /// </summary>
     public Guid Id { get; set; }
-
-    /// <summary>
-    /// The user's full name
-    /// </summary>
-    public string Name { get; set; } = string.Empty;
-
-    /// <summary>
-    /// The user's email address
-    /// </summary>
-    public string Email { get; set; } = string.Empty;
-
-    /// <summary>
-    /// The user's phone number
-    /// </summary>
-    public string Phone { get; set; } = string.Empty;
-
-    /// <summary>
-    /// The user's role in the system
-    /// </summary>
-    public UserRole Role { get; set; }
-
-    /// <summary>
-    /// The current status of the user
-    /// </summary>
-    public UserStatus Status { get; set; }
 }
